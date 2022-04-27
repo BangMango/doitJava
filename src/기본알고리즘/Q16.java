924184c3a19cbc8d80c793eaa5cc2e71a87e244a
BangMango <pjh10207@naver.com>
2022-04-27 17:05:14 +0900

    Q17

---
+++ src/기본알고리즘/Q16.java
@@ -0,0 +1,24 @@
+package 기본알고리즘;
+
+public class Q16 {
+	public static void spira(int n) {
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=(i-1)*2+1;j>=1;j--) {
+				System.out.print("*");
+			}
+			for(int j=n;j>i;j--) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		spira(6);
+
+	}
+
+}
---
+++ src/기본알고리즘/Q17.java
@@ -0,0 +1,25 @@
+package 기본알고리즘;
+
+import java.util.Scanner;
+
+public class Q17 {
+	public static void spiraNum(int n) {
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=(i-1)*2+1;j>=1;j--) {
+				System.out.print(i);
+			}
+			for(int j=n;j>i;j--) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		spiraNum(5);
+	}
+
+}
