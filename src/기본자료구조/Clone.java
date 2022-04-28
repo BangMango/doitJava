417703f91381265a2dad6a1af64262d52aa6c3ae
BangMango <pjh10207@naver.com>
2022-04-28 14:22:08 +0900

    clone

---
+++ src/기본자료구조/Clone.java
@@ -0,0 +1,22 @@
+package 기본자료구조;
+
+public class Clone {
+
+	public static void main(String[] args) {
+		//Clone method : 배열복사
+		int[] a= {1,2,3,4,5};
+		int[] b=a.clone();
+		
+		b[3]=0;
+		System.out.print("a =");
+		for(int i=0;i<a.length;i++) {
+			System.out.print(" "+a[i]);
+		}
+		System.out.print("\nb =");
+		for(int i=0;i<b.length;i++) {
+			System.out.print(" "+b[i]);
+		}
+
+	}
+
+}
